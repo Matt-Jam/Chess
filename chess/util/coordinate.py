837e117175ce1d8b2121f6081e4dd28c8999b0f0
Matt-Jam <mattyjamson@gmail.com>
2025-08-20 21:53:05 +0100

    Added range checks on Coordinate

--- chess/util/coordinate.py
+++ chess/util/coordinate.py
@@ -2,10 +2,15 @@ class Coordinate:
 
     def __init__(self, coord: tuple[int, int]):
         self.coord = coord 
+        self.onBoardCheck()
 
     def __str__(self):
         return f'{chr(self.col+ord("A"))}{8-self.row}'
     
+    def onBoardCheck(self):
+        if self.row < 0 or self.col < 0 or self.row > 7 or self.col > 7:
+            raise ValueError("Coordinate is off the b")
+
     @property
     def row(self):
         return self.coord[0]
